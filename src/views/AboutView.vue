61ade0a71e2db6588e1a9c647d3a75997bb58176
Егор Пантелеев <maikelluri@icloud.com>
2023-05-04 03:11:40 +0300

    fix components

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,11 +1,20 @@
-<script setup>
-import AboutMe from "../components/AboutMe.vue";
-</script>
-
 <template>
-  <section>
-    <div v-motion-slide-visible-once-left>
-      <AboutMe />
-    </div>
-  </section>
+  <el-dialog class="max-w-[600px] min-w-[465px]" v-model="showForm">
+    <Form @close="showForm = false"></Form>
+  </el-dialog>
+  <div v-motion-slide-visible-once-left>
+    <AboutHeaderComponent @showForm="showForm = true"></AboutHeaderComponent>
+    <TestimonialsComponent></TestimonialsComponent>
+    <AboutFaqComponent></AboutFaqComponent>
+  </div>
 </template>
+
+<script lang="ts" setup>
+import AboutFaqComponent from "../components/AboutFaqComponent.vue";
+import TestimonialsComponent from "../components/TestimonialsComponent.vue";
+import AboutHeaderComponent from "../components/AboutHeaderComponent.vue";
+import { ref } from "vue";
+import Form from "../components/Form.vue";
+
+const showForm = ref(false);
+</script>
