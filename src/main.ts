170eb31f40a9960cb93467a24e531d6e15af055e
Егор Пантелеев <kakeparake@gmail.com>
2024-06-19 19:10:19 +0300

    feat: localize element-plus

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,23 @@
-import { MotionPlugin } from "@vueuse/motion";
-import 'animate.css';
-import ElementPlus from 'element-plus';
-import 'element-plus/dist/index.css';
-import 'element-plus/theme-chalk/dark/css-vars.css';
-import { createPinia } from "pinia";
-import { createApp } from "vue";
-import App from "./App.vue";
-import "./index.css";
-import router from "./router";
+import { MotionPlugin } from '@vueuse/motion'
+import 'animate.css'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import ru from 'element-plus/es/locale/lang/ru'
+import 'element-plus/theme-chalk/dark/css-vars.css'
+import { createPinia } from 'pinia'
+import { createApp } from 'vue'
+import App from './App.vue'
+import './index.css'
+import router from './router'
 
-const app = createApp(App);
+const app = createApp(App)
 
-app.use(createPinia());
-app.use(router);
-app.use(MotionPlugin);
-app.use(ElementPlus);
+app.use(createPinia())
+app.use(router)
+app.use(MotionPlugin)
+app.use(ElementPlus, {
+  locale: ru
+})
 
 router.isReady().then(() => {
   app.mount('#app')
